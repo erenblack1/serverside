e64e31085cbcb344f3153d8f3873552cfe38c9fb
erenblack1 <165417559+erenblack1@users.noreply.github.com>
2024-03-30 01:15:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const userRoutes = require('./Routes/userRoutes')
 
 app.use(cors({
-    origin: "https://eshaara.netlify.app"
+    origin: "https://eshaara.netlify.app/"
 }))
 
 app.use((req, res, next) => {
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 // Catch-all route for the frontend
 app.get('*', (req, res) => {
     // Redirect to the frontend URL
-    res.redirect('https://eshaara.netlify.app');
+    res.redirect('https://eshaara.netlify.app/');
 });
 
 app.use(fileUpload())
